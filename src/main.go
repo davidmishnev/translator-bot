91b907f743bf3c5de679fb0f6d6179c6626c3a55
David Mishnev Georgievich <144336674+davidmishnev@users.noreply.github.com>
2025-03-23 00:33:08 +0300

    Update main.go

--- src/main.go
+++ src/main.go
@@ -13,7 +13,6 @@ func main() {
 
 	botToken := "TOKEN"    //bot
 	apiKey := os.Getenv("TOKEN") //api
-	// AQVNy8rAlnAU7SxUvMb0uZqQ1rGPbPAeOhFG1lO3
 	url := "https://translate.api.cloud.yandex.net" //url
 
 	req, err := http.NewRequest("GET", url, nil) //http request
